de2c67359039970853b7ae9f76cd9318f04ceaad
Hakugami <jadenyuki20126@gmail.com>
2024-09-17 01:10:07 +0300

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,52 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
+import java.util.zip.InflaterInputStream;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+//        System.out.println("Logs from your program will appear here!");
 
 //     Uncomment this block to pass the first stage
 
-     final String command = args[0];
+        final String command = args[0];
 
-     switch (command) {
-       case "init" -> {
-         final File root = new File(".git");
-         new File(root, "objects").mkdirs();
-         new File(root, "refs").mkdirs();
-         final File head = new File(root, "HEAD");
+        switch (command) {
+            case "init" -> {
+                final File root = new File(".git");
+                new File(root, "objects").mkdirs();
+                new File(root, "refs").mkdirs();
+                final File head = new File(root, "HEAD");
 
-         try {
-           head.createNewFile();
-           Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
-           System.out.println("Initialized git directory");
-         } catch (IOException e) {
-           throw new RuntimeException(e);
-         }
-       }
-       default -> System.out.println("Unknown command: " + command);
-     }
-  }
+                try {
+                    head.createNewFile();
+                    Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
+                    System.out.println("Initialized git directory");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            case "cat-file" -> {
+                final String hash = args[2];
+                final String folderHash = hash.substring(0, 2);
+                final String fileHash = hash.substring(2);
+                final File blobFile = new File(".git/objects/" + folderHash + "/" + fileHash);
+
+                try(BufferedReader reader = new BufferedReader(new InputStreamReader(new InflaterInputStream(new FileInputStream(blobFile))))) {
+                    String line = reader.readLine();
+                    String formatedLine = line.substring(line.indexOf("\0") + 1); 
+                    System.out.print(formatedLine);
+                    while ((line = reader.readLine()) != null) {
+                        System.out.print(line);
+                    }
+
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+            default -> System.out.println("Unknown command: " + command);
+        }
+    }
 }
